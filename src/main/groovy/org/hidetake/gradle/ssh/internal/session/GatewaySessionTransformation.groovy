147a11a4c020db2e8e0453c8b6babb77257f2797
int128 <int128@gmail.com>
2014-02-23 15:54:46 +0900

    Added multiple gateways support

--- src/main/groovy/org/hidetake/gradle/ssh/internal/session/GatewaySessionTransformation.groovy
+++ src/main/groovy/org/hidetake/gradle/ssh/internal/session/GatewaySessionTransformation.groovy
@@ -8,9 +8,28 @@ import org.hidetake.gradle.ssh.api.SessionSpec
  *
  * @author hidetake.org
  */
-@Singleton
 class GatewaySessionTransformation {
     protected static final LOCALHOST = '127.0.0.1'
+
+    /**
+     * Port forwarding tunnel.
+     */
+    static class Tunnel {
+        final Remote endpoint
+        final SessionSpec spec
+
+        Tunnel(Remote target, Remote gateway) {
+            endpoint = cloneRemote(target)
+            spec = new SessionSpec(gateway, {
+                endpoint.port = forwardLocalPortTo(target.host, target.port)
+            })
+        }
+
+        private static cloneRemote(Remote remote) {
+            new Remote(remote.name, remote.user, 0, LOCALHOST, null,
+                    remote.password, remote.identity, remote.passphrase, remote.agent, remote.roles)
+        }
+    }
 
     /**
      * Applies transformation.
@@ -22,27 +41,41 @@ class GatewaySessionTransformation {
      * @return transformed list
      */
     static List<SessionSpec> transform(List<SessionSpec> specs) {
-        specs.collect { spec ->
-            if (spec.remote.gateway) {
-                def forwardedRemote = new Remote(
-                        spec.remote.name,
-                        spec.remote.user,
-                        0,
-                        LOCALHOST,
-                        null,
-                        spec.remote.password,
-                        spec.remote.identity,
-                        spec.remote.passphrase,
-                        spec.remote.agent,
-                        spec.remote.roles)
-                def forwarder = new SessionSpec(spec.remote.gateway, {
-                    forwardedRemote.port = forwardLocalPortTo(spec.remote.host, spec.remote.port)
-                })
-                def forwarded = new SessionSpec(forwardedRemote, spec.operationClosure)
-                [forwarder, forwarded]
-            } else {
-                spec
-            }
-        }.flatten() as List<SessionSpec>
+        specs.collect(transformer).flatten() as List<SessionSpec>
+    }
+
+    private static transformer = { SessionSpec spec ->
+        if (spec.remote.gateway) {
+            def specs = []
+            def endpoint = generateTunnels(spec.remote, specs)
+            specs << new SessionSpec(endpoint, spec.operationClosure)
+        } else {
+            spec
+        }
+    }
+
+    /**
+     * Recursive generator of tunnels.
+     *
+     * e.g.
+     * <code><pre>
+     *     // Client -> E -> F -> G -> T
+     *     def tunnelF = new Tunnel(target.gateway.gateway, target.gateway.gateway.gateway)
+     *     def tunnelG = new Tunnel(target.gateway, tunnelF.endpoint)
+     *     def tunnelT = new Tunnel(target, tunnelG.endpoint)
+     * </pre></code>
+     *
+     * @param target target remote host
+     * @param specs tunnels
+     * @return endpoint of tunnels
+     */
+    private static generateTunnels(Remote target, List<SessionSpec> specs) {
+        if (target.gateway) {
+            def tunnel = new Tunnel(target, generateTunnels(target.gateway, specs))
+            specs << tunnel.spec
+            tunnel.endpoint
+        } else {
+            target
+        }
     }
 }
